9abf738df47b99f2fcb227033e4aaaa8555027ea
Ian Webster <ianw_github@ianww.com>
2018-05-27 13:16:41 -0400

    Add missing import

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ import json
 
 from flask import jsonify
 from pydruid.client import *
-from pydruid.utils.aggregators import longsum
+from pydruid.utils.aggregators import longsum, doublesum
 
 # Create the application.
 APP = flask.Flask(__name__)
@@ -32,9 +32,9 @@ def run_query():
     #
     #  Task: Replace 'druid_query' with a pydruid query
     # ***************************
-    
+
     druid_query = None
-    
+
     # ***************************
     #   END OF CODE CHANGES
     # ***************************
@@ -49,15 +49,15 @@ def transform_data(data):
     #  Task: Replace the statement below with code that converts the contents of
     #  'data' into the content you want your API endpoint to emit.
     # *************************
-    
-    return None
-    
+
+    return data
+
     # *************************
     #   END OF CODE CHANGES
     # *************************
 
 @APP.route('/get_data')
-def get_data(): 
+def get_data():
     druid_data = run_query()
     response = jsonify(transform_data(druid_data))
     response.headers.add('Access-Control-Allow-Origin', '*')
